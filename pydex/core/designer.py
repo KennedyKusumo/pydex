178f52e788395c42eb657b8236b45bd891a37477
Kennedy Kusumo <29179433+KennedyKusumo@users.noreply.github.com>
2026-03-28 10:22:08 +0000

    Phase 2: wire in DesignerIO mixin, remove duplicate IO methods from designer.py
    - Designer now inherits from DesignerIO
    - Removed 12 duplicate methods: start_logging, stop_logging, show_plots,
      load_oed_result, create_result_dir, write_oed_result, save_state,
      load_state, save_responses, load_sensitivity, load_atomics,
      _generate_result_path
    - Fixed annotated global conflict introduced in Phase 1 (x, pvar)

--- pydex/core/designer.py
+++ pydex/core/designer.py
@@ -28,17 +28,15 @@ from pydex.core.oa.manager import OAManager
 from pydex.core.oa.primal import OAPrimalProblem
 from pydex.core.oa.master import OAMasterProblem
 from pydex.core.logger import Logger
+from pydex.core._mixin_io import DesignerIO
 import matplotlib
 import cvxpy as cp
 import numdifftools as nd
 import numpy as np
 
-# Module-level variables used by plot_prediction_variance's interactive callbacks
-x: NDArray[np.float64]
-pvar: Any
 
 
-class Designer:
+class Designer(DesignerIO):
     """
     An experiment designer with capabilities to do parameter estimation, parameter
     estimability study, and computes both continuous and exact experimental designs.
@@ -3123,14 +3121,6 @@ class Designer:
                     print(self.sampling_times_candidates[i])
         print(f"{'':#^100}")
 
-    def start_logging(self):
-        fn = f"log"
-        fp = self._generate_result_path(fn, "txt")
-        sys.stdout = Logger(file_path=fp)
-
-    def stop_logging(self):
-        sys.stdout = sys.__stdout__
-
     def plot_prediction_variance(self, reso=None, bounds=None, alpha=0.5):
         """
         Plots the prediction variance of the optimal experiment design. To be run after
@@ -3185,8 +3175,6 @@ class Designer:
         y_list = np.array([y1, y2]).transpose()
 
         print("Please select initial control to initialize plot.")
-        x: NDArray[np.float64]
-        pvar: Any
         global x, pvar
         x = np.array(fig1.ginput(1))[0]
         print("Chosen:")
@@ -3315,104 +3303,6 @@ class Designer:
         fig1.canvas.mpl_connect("scroll_event", zoom)
 
         plt.show()
-
-    @staticmethod
-    def show_plots() -> None:
-        plt.show()
-
-    # saving, loading, writing
-    def load_oed_result(self, result_path: str) -> None:
-        with open(getcwd() + result_path, "rb") as file:
-            oed_result = dill.load(file)
-
-        self._optimization_time = oed_result["optimization_time"]
-        self._sensitivity_analysis_time = oed_result["sensitivity_analysis_time"]
-        self._current_criterion = oed_result["optimality_criterion"]
-        self._criterion_value = oed_result["criterion_value"]
-        self.ti_controls_candidates = oed_result["ti_controls_candidates"]
-        self.tv_controls_candidates = oed_result["tv_controls_candidates"]
-        self.model_parameters = oed_result["model_parameters"]
-        self.sampling_times_candidates = oed_result["sampling_times_candidates"]
-        self.efforts = oed_result["optimal_efforts"]
-        self._optimization_package = oed_result["optimization_package"]
-        self._optimizer = oed_result["optimizer"]
-        self._pseudo_bayesian = oed_result["pseudo_bayesian"]
-        self._pseudo_bayesian_type = oed_result["pseudo_bayesian_type"]
-        self._opt_sampling_times = oed_result["optimize_sampling_times"]
-        self._regularize_fim = oed_result["regularized"]
-        self._n_spt_spec = oed_result["n_spt_spec"]
-        self._candidates_changed = False
-        self._model_parameters_changed = False
-
-    def create_result_dir(self) -> None:
-        if self.result_dir_daily is None:
-            now = datetime.now()
-            self.result_dir_daily = getcwd() + "/"
-            self.result_dir_daily += path.splitext(path.basename(main.__file__))[0] + "_result/"
-            self.result_dir_daily += f'date_{now.year:d}-{now.month:d}-{now.day:d}/'
-            self.create_result_dir()
-        else:
-            if path.exists(self.result_dir_daily):
-                return
-            else:
-                makedirs(self.result_dir_daily)
-
-    def write_oed_result(self) -> None:
-        fn = f"{self.oed_result['optimality_criterion']:s}_oed_result"
-        fp = self._generate_result_path(fn, "pkl")
-        dump(self.oed_result, open(fp, "wb"))
-
-    def save_state(self):
-        # pre-process the designer before saving
-        state = [
-            self.n_c,
-            self.n_spt,
-            self.n_r,
-            self.n_mp,
-            self.ti_controls_candidates,
-            self.tv_controls_candidates,
-            self.sampling_times_candidates,
-            self.measurable_responses,
-            self.n_m_r,
-            self.model_parameters,
-        ]
-
-        designer_file = f"state"
-        fp = self._generate_result_path(designer_file, "pkl")
-        dill.dump(state, open(fp, "wb"))
-
-    def load_state(self, designer_path):
-        state = dill.load(open(getcwd() + designer_path, 'rb'))
-        self.n_c = state[0]
-        self.n_spt = state[1]
-        self.n_r = state[2]
-        self.n_mp = state[3]
-        self.ti_controls_candidates = state[4]
-        self.tv_controls_candidates = state[5]
-        self.sampling_times_candidates = state[6]
-        self.measurable_responses = state[7]
-        self.n_m_r = state[8]
-        self.model_parameters = state[9]
-
-    def save_responses(self):
-        # TODO: implement save responses
-        pass
-
-    def load_sensitivity(self, sens_path):
-        self.sensitivities = load(open(getcwd() + "/" + sens_path, "rb"))
-        self._model_parameters_changed = False
-        self._candidates_changed = False
-        return self.sensitivities
-
-    def load_atomics(self, atomic_path):
-        with open(getcwd() + atomic_path, "rb") as file:
-            if self._pseudo_bayesian:
-                self.pb_atomic_fims = load(file)
-            else:
-                self.atomic_fims = load(file)
-        self._model_parameters_changed = False
-        self._candidates_changed = False
-        return self.atomic_fims
 
     """ criteria """
 
@@ -4590,21 +4480,6 @@ class Designer:
         )
 
     """ private methods """
-
-    def _generate_result_path(self, name, extension, iteration=None):
-        self.create_result_dir()
-
-        while True:
-            now = datetime.now()
-            if not self.result_dir:
-                self.result_dir = self.result_dir_daily + f"time_{now.hour:d}-{now.minute:d}-{now.second}/"
-                if not path.exists(self.result_dir):
-                    makedirs(self.result_dir)
-            fn = f"{name}.{extension}"
-            if iteration is not None:
-                fn = f"iter_{iteration:d}_" + fn
-            fp = self.result_dir + fn
-            return fp
 
     def _plot_optimal_sensitivities(self, absolute=False, legend=None,
                                    markersize=10, colour_map="jet",
