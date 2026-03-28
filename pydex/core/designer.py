24d8d2731ef18fe9080532d59635f882ab6f6d8f
Kennedy Kusumo <29179433+KennedyKusumo@users.noreply.github.com>
2026-03-28 11:26:10 +0000

    Phase 2: extract estimability methods into DesignerEstimability mixin
    estimability_study, estimability_study_fim, evaluate_estimability_index,
    and normalize_sensitivities moved from designer.py into _mixin_estimability.py.
    Designer now inherits from DesignerEstimability.

--- pydex/core/designer.py
+++ pydex/core/designer.py
@@ -32,6 +32,7 @@ from pydex.core._mixin_io import DesignerIO
 from pydex.core._mixin_criteria import DesignerCriteria
 from pydex.core._mixin_visualization import DesignerVisualization
 from pydex.core._mixin_apportionment import DesignerApportionment
+from pydex.core._mixin_estimability import DesignerEstimability
 import matplotlib
 import cvxpy as cp
 import numdifftools as nd
@@ -39,7 +40,7 @@ import numpy as np
 
 
 
-class Designer(DesignerIO, DesignerCriteria, DesignerVisualization, DesignerApportionment):
+class Designer(DesignerIO, DesignerCriteria, DesignerVisualization, DesignerApportionment, DesignerEstimability):
     """
     An experiment designer with capabilities to do parameter estimation, parameter
     estimability study, and computes both continuous and exact experimental designs.
@@ -1733,70 +1734,6 @@ class Designer(DesignerIO, DesignerCriteria, DesignerVisualization, DesignerAppo
 
         return self.oed_result
 
-    def estimability_study(self, base_step: float | None = None,
-                           step_ratio: float | None = None,
-                           num_steps: int | None = None,
-                           estimable_tolerance: float = 0.04, write: bool = False,
-                           save_sensitivities: bool = False,
-                           normalize: bool = False) -> NDArray[np.intp]:
-        self._save_sensitivities = save_sensitivities
-        self._compute_sensitivities = self._model_parameters_changed
-        self._compute_sensitivities = self._compute_sensitivities or self._candidates_changed
-        self._compute_sensitivities = self._compute_sensitivities or self.sensitivities is None
-
-        if self._compute_sensitivities:
-            self.eval_sensitivities(
-                base_step=base_step,
-                step_ratio=step_ratio,
-            )
-        if normalize:
-            self.normalize_sensitivities()
-        else:
-            self.normalized_sensitivity = self.sensitivities / self.responses_scales[None, None, :, None]
-
-        z = self.normalized_sensitivity[:, :, self.measurable_responses, :].reshape(
-            self.n_spt * self.n_m_r * self.n_c, self.n_mp)
-
-        z_col_mag = np.nansum(np.power(z, 2), axis=0)
-        next_estim_param = np.argmax(z_col_mag)
-        self.estimable_columns = np.array([next_estim_param])
-        self.estimability = [z_col_mag[next_estim_param]]
-        finished = False
-        while not finished:
-            x_l = z[:, self.estimable_columns]
-            z_theta = np.linalg.inv(x_l.T.dot(x_l)).dot(x_l.T).dot(z)
-            z_hat = x_l.dot(z_theta)
-            r = z - z_hat
-            r_col_mag = np.nansum(np.power(r, 2), axis=0)
-            next_estim_param = np.argmax(r_col_mag)
-            if r_col_mag[next_estim_param] <= estimable_tolerance:
-                if write:
-                    fn = f"estimability_{self.n_c}_cand"
-                    fp = self._generate_result_path(fn, "pkl")
-                    dump(self.estimable_columns, open(fp, 'wb'))
-                print(
-                    f'Identified estimable parameters are: '
-                    f'{np.array2string(self.estimable_columns,separator=", ")}'
-                )
-                with np.printoptions(precision=1):
-                    print(
-                        f"Degree of Estimability: "
-                        f"{np.array2string(np.asarray(self.estimability),separator=', ')}"
-                    )
-                return self.estimable_columns
-            self.estimability.append(r_col_mag[next_estim_param])
-            self.estimable_columns = np.append(self.estimable_columns, next_estim_param)
-        return self.estimable_columns  # unreachable: loop exits via return inside
-
-    def estimability_study_fim(self, save_sensitivities=False):
-        self._save_sensitivities = save_sensitivities
-        self.efforts = np.ones((self.n_c, self.n_spt))
-        self.eval_fim(self.efforts)
-        print(f"Estimable parameters: {self.estimable_model_parameters}")
-        with np.printoptions(precision=1):
-            print(f"Degree of Estimability: {self.estimability}")
-        return self.estimable_model_parameters, self.estimability
-
     """ core utilities """
 
 
@@ -2525,89 +2462,6 @@ class Designer(DesignerIO, DesignerCriteria, DesignerVisualization, DesignerAppo
         return self.optimal_candidates
 
     """ optional operations """
-
-    def evaluate_estimability_index(self):
-        self.estimable_model_parameters = np.array([])
-        self.estimability = np.array([])
-        if self._optimization_package == 'cvxpy':
-            try:
-                fim_value = self.fim.value
-            except AttributeError:
-                fim_value = self.fim
-        else:
-            fim_value = self.fim
-        try:
-            if isinstance(self.fim, cp.Expression):
-                if np.all(self.fim.value == 0):
-                    return
-            else:
-                if np.all(self.fim == 0):
-                    return
-        except AttributeError:
-            if isinstance(self.fim, cp.expressions.expression.Expression):
-                if np.all(self.fim.value == 0):
-                    return
-            else:
-                if np.all(self.fim == 0):
-                    return
-
-        for i, row in enumerate(fim_value):
-            if not np.allclose(row, 0.0):
-                self.estimable_model_parameters = np.append(
-                    self.estimable_model_parameters, i)
-                self.estimability = np.append(self.estimability,
-                                              np.sqrt(np.inner(row, row)))
-        self.estimable_model_parameters = self.estimable_model_parameters.astype(int)
-
-        if self._trim_fim:
-            if len(self.estimable_model_parameters) == 0:
-                self.fim = np.array([0])
-            else:
-                self.fim = self.fim[
-                    np.ix_(self.estimable_model_parameters, self.estimable_model_parameters)
-                ]
-
-    def normalize_sensitivities(self, overwrite_unnormalized=False):
-        assert not np.allclose(self.model_parameters,
-                               0), 'At least one nominal model parameter value is ' \
-                                   'equal to 0, cannot normalize sensitivities. ' \
-                                   'Consider re-estimating your parameters or ' \
-                                   're-parameterize your model.'
-
-        # normalize parameter values
-        self.normalized_sensitivity = np.multiply(self.sensitivities,
-                                                  self.model_parameters[None, None, None,
-                                                  :])
-        if self.responses_scales is None:
-            if self._verbose >= 0:
-                print(
-                    'Scale for responses not given, using raw prediction values to '
-                    'normalize sensitivities; '
-                    'likely to fail (e.g. if responses are near 0). Recommend: provide '
-                    'designer with scale '
-                    'info through: "designer.responses_scale = <your_scale_array>."')
-            if self.response is None:
-                self.simulate_candidates(store_predictions=True)
-            # normalize response values
-            self.normalized_sensitivity = np.divide(
-                self.normalized_sensitivity,
-                self.response[:, :, :, None],
-            )
-        else:
-            assert isinstance(self.responses_scales,
-                              np.ndarray), "Please specify responses_scales as a 1D " \
-                                           "numpy array."
-            assert self.responses_scales.size == self.n_r, 'Length of responses scales ' \
-                                                           'those which are measurable ' \
-                                                           'and not).)'
-            self.normalized_sensitivity = np.divide(self.normalized_sensitivity,
-                                                    self.responses_scales[None, None, :,
-                                                    None])
-        if overwrite_unnormalized:
-            self.sensitivities = self.normalized_sensitivity
-            self._sensitivity_is_normalized = True
-            return self.sensitivities
-        return self.normalized_sensitivity
 
     """ private methods """
 
